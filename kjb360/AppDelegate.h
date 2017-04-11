6dfbcd1ea185e4c8631abc728d9157b9f077fa36
ytx <ytx123013@gmail.com>
2017-04-11 15:47:40 +0800

    Initial Commit

---
+++ kjb360/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  kjb360
+//
+//  Created by aaa on 2017/4/11.
+//  Copyright © 2017年 ytx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
