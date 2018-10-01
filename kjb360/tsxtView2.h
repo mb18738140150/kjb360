5737b8affcd0bf69218a843862f31b2e71031bc5
qiushuang <1306503317@qq.com>
2018-10-01 10:29:20 +0800

    testview2

---
+++ kjb360/tsxtView2.h
@@ -0,0 +1,17 @@
+//
+//  tsxtView2.h
+//  kjb360
+//
+//  Created by FRANKLIN on 2018/10/1.
+//  Copyright © 2018 ytx. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface tsxtView2 : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ kjb360/tsxtView2.m
@@ -0,0 +1,13 @@
+//
+//  tsxtView2.m
+//  kjb360
+//
+//  Created by FRANKLIN on 2018/10/1.
+//  Copyright © 2018 ytx. All rights reserved.
+//
+
+#import "tsxtView2.h"
+
+@implementation tsxtView2
+
+@end
