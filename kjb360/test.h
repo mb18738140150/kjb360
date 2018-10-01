b8db3dce9f3a1f7d8e4f8faa4c44a307b3727fde
ytx <ytx123013@gmail.com>
2018-10-01 10:24:53 +0800

    test3

---
+++ kjb360/test.h
@@ -0,0 +1,13 @@
+//
+//  test.h
+//  kjb360
+//
+//  Created by aaa on 2018/10/1.
+//  Copyright © 2018年 ytx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface test : UIView
+
+@end
---
+++ kjb360/test.m
@@ -0,0 +1,21 @@
+//
+//  test.m
+//  kjb360
+//
+//  Created by aaa on 2018/10/1.
+//  Copyright © 2018年 ytx. All rights reserved.
+//
+
+#import "test.h"
+
+@implementation test
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
